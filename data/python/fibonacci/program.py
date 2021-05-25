0c165c3e9677998649e2e541d7dab84f47814633
Frederik Schmitt <s8fkschm@stud.uni-saarland.de>
2021-05-25 12:40:44 +0200

    typos

--- data/python/fibonacci/program.py
+++ data/python/fibonacci/program.py
@@ -1,4 +1,4 @@
 n = int(input("Enter n: "))
 def fibo(x):
     return x if x <= 1 else fibo(x - 1) + fibo(x - 2)
-print(f"The n-th fibonacci number is {fibo(n)}")
+print(f"The n-th Fibonacci number is {fibo(n)}")
